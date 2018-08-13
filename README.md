819083aff00ba5334df470bf1bef14284e21edba
lyk2655 <lyk2655@126.com>
2018-08-13 23:58:19 +0800

    face detection add Image for example in README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # face-detection
-face detection with opencv in java
+face detection with opencv3.2.0 in java
+本项目采用penCV实现人脸检测
+![人脸检测示例](https://github.com/lyk2655/face-detection/blob/master/zhuyin.png)
--- src/com/linyk3/facedetection/FaceDetector.java
+++ src/com/linyk3/facedetection/FaceDetector.java
@@ -1,4 +1,4 @@
-package com.shekhar.facedetection;
+package com.linyk3.facedetection;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,7 +17,7 @@ import org.opencv.objdetect.CascadeClassifier;
 public class FaceDetector {
 
 	private static int imgCount = 0;
-        public static void main(String[] args) {
+    public static void main(String[] args) {
     	//导入opencv,本项目采用的是opencv3.2.0
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("FaceDetector begin");
@@ -26,38 +26,44 @@ public class FaceDetector {
         String xmlPath = FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1) ; 
         //System.out.println(xmlPath);
         
-        //检测的照片
+        //检测的照片路径
         String srcPath = FaceDetector.class.getResource("/img/src/").getPath().substring(1);
-        //处理完照片存储路径		
-	String dstPath = FaceDetector.class.getResource("/img/dst/").getPath().substring(1);
+        //检测后保存的路径
+        String dstPath = FaceDetector.class.getResource("/img/dst/").getPath().substring(1);
         //System.out.println(srcPath);
         
-        //String imgsrc = srcPath+"zhuyin.JPG";
-        String imgsrc = srcPath+"many.JPG";
+        String imgsrc = srcPath+"zhuyin.JPG";
+        
         System.out.println("detection img: " + imgsrc);
+        //核心：加载xml识别配置文件，将图像转化为Mat对象后利用OpenCV函数进行识别，将识别到的人脸保存到 faceDetections
         CascadeClassifier faceDetector = new CascadeClassifier(xmlPath);
         Mat image = Imgcodecs.imread(imgsrc);
-
         MatOfRect faceDetections = new MatOfRect();
         faceDetector.detectMultiScale(image, faceDetections);
 
         System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
 
+        SimpleDateFormat formatter  = new SimpleDateFormat("yyyyMMddHHmmss");
+        String date = formatter.format(new Date());
         for (Rect rect : faceDetections.toArray()) {
         	//画出人脸方框
-            //Imgproc.rectangle(image, new Point(rect.x, rect.y),new Point(rect.x + rect.width, rect.y + rect.height),new Scalar(0, 255, 255));
-
+            Imgproc.rectangle(image, new Point(rect.x, rect.y),new Point(rect.x + rect.width, rect.y + rect.height),new Scalar(0, 255, 255));
+            
+            //将识别到的人脸剪切，并重新设置大小
             Rect roi = new Rect(rect.x, rect.y, rect.width, rect.height);
             Mat tmp = new Mat(image, roi);
             Mat dst = tmp.clone();
             Imgproc.resize(tmp, dst, new Size(160, 160));
-            SimpleDateFormat formatter  = new SimpleDateFormat("yyyyMMddHHmmss");
-            String date = formatter.format(new Date());
+            
+            // 将人脸保存
             String filename = dstPath + date +  imgCount +".png";
             imgCount++;
             System.out.println(String.format("Writing %s", filename));
             Imgcodecs.imwrite(filename, dst);
-
         }
+        String filename = dstPath + date +".png";
+        System.out.println(String.format("Writing %s", filename));
+        Imgcodecs.imwrite(filename, image);
+        
     }
 }
